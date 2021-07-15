3e4db73bec01397dcbf9e0c7f33911c6588a6cc6
Chivu Marius <chivumarius@yahoo.com>
2021-07-15 11:54:43 +0300

    Prototipuri (Obiecte Parinte): Evitarea Extinderi (Modificarii) Obiectelor Incorporate in JavaScript

---
+++ js/prototipuri/prototipuri_(obiecte_parinte)___evitarea_extinderi_(modificarii)_obiectelor_incorporate_in_js.js
@@ -0,0 +1,25 @@
+/* ===============================================================
+   PROTOTIPURI (OBIECTE PARINTE):
+   EVITAREA  EXTINDERII  (MODIFICARII)  OBIECTELOR  INCORPORATE  IN  JAVASCRIPT
+=================================================================*/
+
+/*
+
+    NB! 
+        'NU TREBUIE' SA 'MODIFICAM' OBIECTELE 'BUILT-IN' (INCORPORATE)
+            IN 'JAVASCRIPT'.
+*/
+
+//=================================================================
+// (#) MET. 'SHUFFLE' ('AMESTECARE'):
+//=================================================================
+
+Array.prototype.shuffle = function() {
+    // ...
+}
+
+// DEFINIM MATRICEA:
+const matrice = [];
+
+// APELARE METODA DE AMESTECARE:
+matrice.shuffle();
